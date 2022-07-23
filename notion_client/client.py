ea53319ce36b04cd3e964a58f930834e7dd63bb2
JezaChen <jeza@vip.qq.com>
2022-07-23 21:58:56 +0800

    First Commit: Basic Structure Finished

---
+++ notion_client/client.py
@@ -0,0 +1,96 @@
+# -*- coding:utf-8 _*-
+""" 
+@author: Jeza Chen
+@license: GPL-3.0 License
+@file: client.py 
+@time: 2022/07/23
+@contact: jeza@vip.qq.com
+@site:  
+@software: PyCharm 
+@description: 
+"""
+import functools
+import json
+from dataclasses import dataclass
+import typing
+from typing import Optional, Union
+
+import httpx
+
+from notion_client.errors import raise_api_response_exception_by_err_code, UnknownAPIResponseError
+
+
+@dataclass(frozen=True)
+class ClientOptions:
+    # `auth_token` is required option
+    auth_token: str
+    # `notion_version` specifies the API version
+    notion_version: str = "2022-06-28"
+    base_url = "https://api.notion.com/v1"
+
+
+class Client:
+    __http_client: httpx.Client
+
+    def __init__(
+            self,
+            options: Optional[Union[ClientOptions, typing.Dict]] = None,
+            **kwargs
+    ):
+        if options is None:
+            options = ClientOptions(**kwargs)
+        elif isinstance(options, dict):
+            options = ClientOptions(**options)
+        self.__options = options
+
+        self.__http_client = httpx.Client(
+            base_url=self.__options.base_url,
+            headers={
+                "Authorization": f"Bearer {self.__options.auth_token}",
+                "Notion-Version": self.__options.notion_version
+            }
+        )
+
+    def _make_request(self, method: str, path: str, query: str, body: str):
+        return self.__http_client.build_request(method, path, params=query, json=body)
+
+    def _parse_response(self, rsp: httpx.Response):
+        try:
+            rsp.raise_for_status()
+        except httpx.HTTPStatusError as err:
+            try:
+                body = err.response.json()
+                err_code = body.get("code")
+                err_detail = body.get("message", "")
+            except json.JSONDecoder:
+                err_code = None
+                err_detail = ""
+
+            # must raise
+            if err_code is not None:
+                raise_api_response_exception_by_err_code(
+                    err_code,
+                    err_detail
+                )
+            else:
+                raise UnknownAPIResponseError(err_detail)
+
+        ret = rsp.json()
+        return ret
+
+    def request(self,
+                method: str,
+                path: str,
+                query: Optional[str] = None,
+                body: Optional[str] = None):
+        req = self._make_request(method, path, query, body)
+        rsp = self.__http_client.send(req)
+        return self._parse_response(rsp)
+
+    # Specific Request Methods
+    get = functools.partialmethod(request, "get")
+    post = functools.partialmethod(request, "post")
+    patch = functools.partialmethod(request, "patch")
+    delete = functools.partialmethod(request, "delete")
+
+
---
+++ notion_client/errors.py
@@ -0,0 +1,166 @@
+# -*- coding:utf-8 _*-
+""" 
+@author: Jeza Chen
+@license: GPL-3.0 License
+@file: errors.py 
+@time: 2022/07/23
+@contact: jeza@vip.qq.com
+@site:  
+@software: PyCharm 
+@description: 
+"""
+from typing import Optional
+
+_MAPPING_ERR_CODE_TO_API_RESPONSE_ERROR_CLASS = {}
+
+
+class _PyNotionAPIResponseExceptionMeta(type):
+    def __new__(cls, name, bases, namespace):
+        exc_cls = super().__new__(cls, name, bases, namespace)
+        err_code = namespace.get("err_code")
+        is_excluded = namespace.get("__is_excluded__", False)
+
+        if not is_excluded:
+            if not err_code:
+                raise NotImplementedError(f"Defining class {name} requires err_code field.")
+            _MAPPING_ERR_CODE_TO_API_RESPONSE_ERROR_CLASS[err_code] = exc_cls
+        return exc_cls
+
+
+class PyNotionBaseException(Exception):
+    pass
+
+
+class PyNotionAPIResponseException(PyNotionBaseException,
+                                   metaclass=_PyNotionAPIResponseExceptionMeta):
+    """ Exception base class for marking various errors in API responses
+    """
+
+    err_message: str = ""
+    err_code: str = ""
+
+    # if set `__is_excluded` to true, the class will not be added into _MAPPING_ERR_CODE_TO_API_RESPONSE_ERROR_CLASS
+    __is_excluded__ = True
+
+    def __init__(self, err_detail: Optional[str] = None):
+        self.err_detail = err_detail
+
+    def __str__(self):
+        return f"{self.err_message}\nMore details returned by the Notion API:\n{self.err_detail}"
+
+
+class InvalidJsonError(PyNotionAPIResponseException):
+    """ The request body could not be decoded as JSON.
+    """
+    err_message = "The request body could not be decoded as JSON."
+    err_code = "invalid_json"
+
+
+class InvalidRequestUrlError(PyNotionAPIResponseException):
+    """ The request URL is not valid.
+    """
+    err_message = "The request URL is not valid."
+    err_code = "invalid_request_url"
+
+
+class InvalidRequestError(PyNotionAPIResponseException):
+    """ This request is not supported.
+    """
+    err_message = "This request is not supported."
+    err_code = "invalid_request"
+
+
+class ValidationError(PyNotionAPIResponseException):
+    """ The request body does not match the schema for the expected parameters. 
+    Check the "message" property for more details.
+    """
+    err_message = "The request body does not match the schema for the expected parameters."
+    err_code = "validation_error"
+
+
+class MissingVersionError(PyNotionAPIResponseException):
+    """ The request is missing the required Notion-Version header.
+    See Versioning(https://developers.notion.com/reference/versioning).
+    """
+    err_message = "The request is missing the required Notion-Version header."
+    err_code = "missing_version"
+
+
+class UnauthorizedError(PyNotionAPIResponseException):
+    """ The bearer token is not valid.
+    """
+    err_message = "The bearer token is not valid."
+    err_code = "unauthorized"
+
+
+class RestrictedResourceError(PyNotionAPIResponseException):
+    """ Given the bearer token used, the client doesn't have permission to perform this operation.
+    """
+    err_message = "Given the bearer token used, the client doesn't have permission to perform this operation."
+    err_code = "restricted_resource"
+
+
+class ObjectNotFoundError(PyNotionAPIResponseException):
+    """ Given the bearer token used, the resource does not exist.
+    This error can also indicate that the resource has not been shared with owner of the bearer token.
+    """
+    err_message = "The resource does not exist."
+    err_code = "object_not_found"
+
+
+class ConflictError(PyNotionAPIResponseException):
+    """ The transaction could not be completed, potentially due to a data collision.
+    Make sure the parameters are up to date and try again.
+    """
+    err_message = "The transaction could not be completed, potentially due to a data collision. " \
+                  "Make sure the parameters are up to date and try again."
+    err_code = "conflict_error"
+
+
+class RateLimitedError(PyNotionAPIResponseException):
+    """ This request exceeds the number of requests allowed.
+    Slow down and try again. More details on rate limits(https://developers.notion.com/reference/errors#request-limits).
+    """
+    err_message = "This request exceeds the number of requests allowed." \
+                  "Slow down and try again." \
+                  " More details on rate limits(https://developers.notion.com/reference/errors#request-limits)."
+    err_code = "rate_limited"
+
+
+class InternalServerError(PyNotionAPIResponseException):
+    """ An unexpected error occurred.
+    Reach out to Notion support
+    (https://www.notion.so/notion/Notion-Official-83715d7703ee4b8699b5e659a4712dd8?target=intercom).
+    """
+    err_message = "An unexpected error occurred."
+    err_code = "internal_server_error"
+
+
+class ServiceUnavailableError(PyNotionAPIResponseException):
+    """ Notion is unavailable. Try again later.
+    This can occur when the time to respond to a request takes longer than 60 seconds, the maximum request timeout.
+    """
+    err_message = "Notion is unavailable. Try again later."
+    err_code = "service_unavailable"
+
+
+class DatabaseConnectionUnavailableError(PyNotionAPIResponseException):
+    """ Notion's database is unavailable or in an unqueryable state. Try again later.
+    """
+    err_message = "Notion's database is unavailable or in an unqueryable state. Try again later."
+    err_code = "database_connection_unavailable"
+
+
+class UnknownAPIResponseError(PyNotionAPIResponseException):
+    """ A Response Error which is not included in Notion Official Errors.
+    (https://developers.notion.com/reference/errors)
+    """
+    err_message = "Unknown API response error occurs."
+    __is_excluded__ = True
+
+
+def raise_api_response_exception_by_err_code(err_code: str, err_detail: str):
+    exp_cls = _MAPPING_ERR_CODE_TO_API_RESPONSE_ERROR_CLASS[err_code]
+    if exp_cls is None:
+        raise UnknownAPIResponseError(err_detail)
+    raise exp_cls(err_detail)
